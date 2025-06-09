48c54cabec902cf83f53765c7c0e0a7fa2cbfd3a
M. Sherafati <36226207+herman72@users.noreply.github.com>
2025-06-09 13:34:09 +0300

    feat: Add CI workflow for Terraform

--- 01-infrastructure/main.tf
+++ 01-infrastructure/main.tf
@@ -41,7 +41,7 @@ module "eks" {
   version = "20.8.4"
 
   cluster_name    = "${var.project_name}-cluster"
-  cluster_version = "1.30"
+  cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
